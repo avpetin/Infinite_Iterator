fbbe29af6b535470ff34eddc59b338e515d5c139
Alexey Petin <alex_beholder@mail.ru>
2023-05-24 22:28:26 +0300

    Update3

--- src/main/java/Randoms.java
+++ src/main/java/Randoms.java
@@ -1,11 +1,8 @@
-import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Random;
 
 public class Randoms implements Iterable<Integer> {
     protected Random random = new Random();
-    private final List<Integer> randomNumbers = new ArrayList<>();
     private final int min, max;
 
     public Randoms(int min, int max) {
@@ -18,15 +15,12 @@ public class Randoms implements Iterable<Integer> {
         return new Iterator<>() {
             @Override
             public boolean hasNext() {
-                randomNumbers.add(random.nextInt((max - min) + 1) + min);
                 return true;
             }
 
             @Override
             public Integer next() {
-                int value = randomNumbers.get(0);
-                randomNumbers.remove(0);
-                return value;
+                return random.nextInt((max - min) + 1) + min;
             }
         };
     }
